b6231d65ee23ea2176e9929df3f9bc969049267f
zutken <b.alzamora@gmail.com>
2017-04-26 17:12:41 +1000

    updating history format

--- basics.py
+++ basics.py
@@ -1,4 +1,5 @@
 from bitcoin import *
+from blockchain import *
 priv = sha256("bitcoin is awesome lets all go to the moon someday")
 print("sha256 private key : "+str(priv))
 print("================================================")
@@ -11,6 +12,10 @@ print("bitcoin address : "+str(addr))
 print("================================================")
 print("================================================")
 
+
 file = open("history.txt", "w")
-file.write(history("1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX"))
+for r in history("1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX"):
+    file.write(str(r)+"\n")
 file.close()
+
+
